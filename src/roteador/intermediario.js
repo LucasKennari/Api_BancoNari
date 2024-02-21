8e5da8f9179b9a7dc5e8c9cc5b2811ce79b9c6d2
Lucas Cezar <lucas.oheisen@gmail.com>
2024-02-21 17:02:38 -0300

    -

--- src/roteador/intermediario.js
+++ src/roteador/intermediario.js
@@ -1,21 +1,8 @@
-const { banco } = require('../bancoDeDados/bancodedados')
-let { senha } = banco
-
+const knex = require("../bancoDeDados/conexao");
+let { senha } = banco;
 
 const checarSenha = async (req, res, next) => {
-    const { senha_banco } = req.query
+     //jwt
+};
 
-    if (senha_banco === senha) {
-        return next()
-    }
-
-    if (String(senha_banco)) {
-        return res.status(403).json({ mensagem: "A senha do banco informada é inválida!" })
-    }
-    if (!senha_banco) {
-        return res.status(400).json({ mensagem: 'informe a senha!' })
-    }
-
-}
-
-module.exports = { checarSenha }
+module.exports = { checarSenha };
