6213cb1996c788bb1a5820290a7c87517aba43c1
Lucas Cezar <lucas.oheisen@gmail.com>
2024-02-21 17:00:26 -0300

    corrigido a descricao de testes de 'TENTAR criar' para 'NÃOCRIAR'

--- tests/conta/cadastro.test.js
+++ tests/conta/cadastro.test.js
@@ -7,18 +7,28 @@ describe("Conta - Cadastro", () => {
                login: "teste02001",
                nome: "testJ2est",
                email: "test020e@teste.com",
-               senha: "teste12223456",
+               senha: "1234567890",
           });
           expect(res1.status).toEqual(201);
           expect(typeof res1.body).toEqual("number");
      });
 
-     it("Tentar criar outra conta com o mesmo email", async () => {
+     it("NÃO criar outra conta com o mesmo login", async () => {
           const res1 = await testServer.post("/cadastro").send({
-               login: "teste0001",
+               login: "teste02001",
                nome: "testJest",
-               email: "test00e@teste.com",
-               senha: "teste123456",
+               email: "test020ew22@teste.com",
+               senha: "1234567890",
+          });
+          expect(res1.status).toEqual(400);
+          expect(res1.body).toHaveProperty("mensagem");
+     });
+     it("NÃO criar outra conta com o mesmo email", async () => {
+          const res1 = await testServer.post("/cadastro").send({
+               login: "teste00012",
+               nome: "testJest",
+               email: "test020e@teste.com",
+               senha: "1234567890",
           });
           expect(res1.status).toEqual(400);
           expect(res1.body).toHaveProperty("mensagem");
